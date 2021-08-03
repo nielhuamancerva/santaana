4ba40df05e9acc839f61c11fd6e1a930cf4faf26
nielhuaman <73756567+nielhuaman@users.noreply.github.com>
2021-08-03 08:12:53 -0500

    Version 1.0.3
    Version 1.0.3

--- src/app/modules/users/components/edit-user-modal/edit-user-modal.component.ts
+++ src/app/modules/users/components/edit-user-modal/edit-user-modal.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgbActiveModal, NgbDateAdapter, NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-edit-user-modal',
   templateUrl: './edit-user-modal.component.html',
   styleUrls: ['./edit-user-modal.component.scss']
 })
 export class EditUserModalComponent implements OnInit {
+  
+  isLoading$;
+
+  formGroup: FormGroup;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  
+   // helpers for View
+   isControlValid(controlName: string): boolean {
+    const control = this.formGroup.controls[controlName];
+    return control.valid && (control.dirty || control.touched);
+  }
+
+  isControlInvalid(controlName: string): boolean {
+    const control = this.formGroup.controls[controlName];
+    return control.invalid && (control.dirty || control.touched);
+  }
+
+  controlHasError(validation, controlName): boolean {
+    const control = this.formGroup.controls[controlName];
+    return control.hasError(validation) && (control.dirty || control.touched);
+  }
+
+  isControlTouched(controlName): boolean {
+    const control = this.formGroup.controls[controlName];
+    return control.dirty || control.touched;
+  }
 }
--- src/app/modules/users/users.module.ts
+++ src/app/modules/users/users.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { InlineSVGModule } from 'ng-inline-svg';
 import { ListUserComponent } from './components/list-user/list-user.component';
+import { CRUDTableModule } from '../../_metronic/shared/crud-table';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { UsersRoutingModule } from './users-routing.module';
 
 
 @NgModule({
   declarations: [ListUserComponent],
   imports: [
-    CommonModule,UsersRoutingModule
+    CommonModule,HttpClientModule,InlineSVGModule,CRUDTableModule,FormsModule,ReactiveFormsModule,UsersRoutingModule
   ]
 })
 export class UsersModule { }
